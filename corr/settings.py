5a9e67b90d9204d989f63840cff0c4097f392dd9
hannahmhaesuarezlaquinon <namhaelaquinon@gmail.com>
2022-04-05 15:47:29 +0800

    2

--- corr/settings.py
+++ corr/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+
 import os
 from pathlib import Path
 
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0ive^_1v(zrvk4^z@x@w8ylm@zd-(xjk@+)aac0hwu3%y#=57$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = FALSE
+DEBUG = True
 
-ALLOWED_HOSTS = [group9-aloconferenceroomreservationsystem.azurewebsites.net]
+ALLOWED_HOSTS = ['group9-aloconferenceroomreservationsystem.azurewebsites.net']
 
 
 # Application definition
@@ -37,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'corr',
 ]
 
@@ -125,7 +125,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = BASE_DIR/'staticfiles'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static') 
+] 
 
 MEDIA_URL = '/images/'
 
